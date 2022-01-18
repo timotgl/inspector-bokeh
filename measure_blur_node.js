11c6f2b1efb01fac268fe92c55b43d7d2c071a4e
Timo Taglieber <github@timotaglieber.de>
2022-01-18 11:37:53 +0100

    Update measure_blur_node.js to use updated canvas module and const and let instead of var

--- measure_blur_node.js
+++ measure_blur_node.js
@@ -1,37 +1,27 @@
-var fs = require('fs');
-var Canvas = require('canvas');
-var measureBlur = require('./measure_blur.js')
-var image;
+const { createCanvas, loadImage } = require('canvas')
+const measureBlur = require('./measure_blur.js')
+let image;
 
-function createImage(error, data) {
-    if (error) {
-        console.error('Unable to read image file!');
-        throw error;
-    }
-    image = new Canvas.Image;
-    image.onload = drawImageOnCanvas;
-    image.src = data;
+function createImage(imageData) {
+    image = imageData;
+    drawImageOnCanvas();
 }
 
 function drawImageOnCanvas() {
-    var canvas = new Canvas(),
-        context;
-
-    canvas.width = image.width;
-    canvas.height = image.height;
-    context = canvas.getContext('2d');
+    const canvas = createCanvas(image.width, image.height);
+    const context = canvas.getContext('2d');
     context.drawImage(image, 0, 0);
 
     showBlurScore(context.getImageData(0, 0, canvas.width, canvas.height))
 }
 
 function showBlurScore(imageData) {
-    var stats = measureBlur(imageData);
+    const stats = measureBlur(imageData);
     console.log('Blur score:', Number((stats.avg_edge_width_perc).toFixed(2)));
     console.log(stats);
 }
 if (process.argv.length >= 3) {
-    fs.readFile(process.argv[2], createImage);
+    loadImage(process.argv[2]).then(createImage);
 } else {
     console.info('Usage: node measure_blur.js path/to/image/file.jpg');
 }
